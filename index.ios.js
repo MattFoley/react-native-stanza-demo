590084a3ba36606b0cd4dc2f08eca98b72164917
Tj <tfallon@mail.depaul.edu>
2017-01-30 18:29:10 -0800

    Backing up and committing shim file which replaces crypto.getRandomValues.

--- index.ios.js
+++ index.ios.js
@@ -16,6 +16,70 @@ import {
 import XMPP from 'stanza.io';
 
 export default class ReactNativeStanzaDemo extends Component {
+  constructor(props) {
+    super(props);
+    //This is tested using a locally configured ejabberd docker container
+    let client = XMPP.createClient({
+      jid: 'test@localhost',
+      password: 'password',
+
+      // If you have a .well-known/host-meta.json file for your
+      // domain, the connection transport config can be skipped.
+      transport: 'websocket',
+      wsURL: 'ws://localhost:5280/http-ws/'
+      // (or `boshURL` if using 'bosh' as the transport)
+    });
+
+    client.connect();
+
+    client.on('session:started', () => {
+      console.log('Client Connected');
+    });
+
+    client.on('raw:incoming', (xml) => {
+      console.log('xml received: ' + xml);
+    });
+
+    client.on('raw:outgoing', (xml) => {
+      console.log('xml sent:' + xml);
+    });
+
+    client.on('stream:data', (thing) => {
+      console.log('stream:data' + thing);
+    });
+
+    client.on('auth:failed', (xml) => {
+      console.log('Auth Failed ');
+    });
+
+    client.on('connected', () => {
+      console.log('connected');
+    });
+
+    client.on('disconnected', (xml) => {
+      console.log('disconnected ' + Object.keys(xml));
+    });
+
+    client.on('connected', (xml) => {
+      console.log('connected ' + Object.keys(xml));
+    })
+
+    client.on('stream:error', () => {
+      console.log('stream:error');
+    });
+
+    client.on('session:error', () => {
+      console.log('session:error');
+    });
+
+    console.log('Tried creating a client');
+
+  //  client.connect();
+    console.log('Tried connecting to a client');
+
+    //this.client = client;
+  }
+
   render() {
     return (
       <View style={styles.container}>
